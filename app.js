fe26c49cd7327f894a2c9d505eb83e9693bf0a2b
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2015-05-15 15:32:37 +0200

    added city

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify({cities : ["San Francisco","Amsterdam", "Berlin", "New York", "Palo ALto"]}));
+    res.write(JSON.stringify({cities : ["San Francisco","Amsterdam", "Berlin", "New York", "Palo ALto", "San Mateo"]}));
     res.end();
 });
 
