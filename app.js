2b7b8f691baab01e992fd9a9b3fca70fdbeafb95
pghildiy <pghildiyal82@gmail.com>
2020-02-11 15:34:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo"]}
+var cities = {cities:["Amsterdam","Berlin","New York","San Francisco","Tokyo", "Delhi"]}
 
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
