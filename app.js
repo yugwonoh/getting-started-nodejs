e51fb95cf9e957d81ef4c12b7a50b1ec5605aad3
pghildiy <pghildiyal82@gmail.com>
2020-05-07 14:34:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ setInterval(function(str1, str2) {
     var text = makeid(40);
     var ran = Math.random()
     if (ran > 0.75)
-        text = makeid(40)+ " "+makeid(40)+ " "+makeid(40)+ " "+makeid(40)
+        text = makeid(40)+ " "+makeid(40)+ " \n "+makeid(40)+ " "+makeid(40)
     else if (ran > 0.5)
         text = makeid(40)+ " "+makeid(40)+ " "+makeid(40)
     else if (ran > 0.25)
