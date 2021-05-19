e167406d6e897263299734341f20a0e81db31387
pghildiy <pghildiyal82@gmail.com>
2021-05-19 10:27:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var text1 = makeid(40);
 var text2 = text1+ " "+makeid(40);
 var text3 = text2+ " "+makeid(40);
 var text4 = text3+ " "+makeid(40);
+var logs = ["i 1", "am 1", "here 1", "hello 1", "and 1", "you 1", "know 1", "who 1", "i 2", "am 2", "here 2", "hello 2", "hello 2", "and 2", "you 2", "know 2", "who 2", "i 3", "am 3", "here 3", "hello 3", "and 3", "hello 3", "and 3", "you 3", "know 3", "who 3", "i 4", "am 4", "here 4", "hello 4", "and 4", "we 4", "hello 4", "and 4", "you 4", "know 4", "who 4", "i 5", "am 5", "here 5", "hello 5", "or 5", "not 5", "and 5", "we 5", "hello 5", "and 5", "you 5", "know 5", "who 5", "i 6", "am 6", "here 6", "hello 6", "or 6", "not 6", "and 6", "hello 6", "and 6", "you 6", "know 6", "who 6", "i 7", "am 7", "here 7", "hello 7", "and 7", "we 7", "are 7", "here1 7", "hello 7", "and 7", "you 7", "know 7", "who 7", "i 8", "am 8", "here 8", "hello 8", "and 8", "we 8", "miss 8", "are 8", "here1 8", "hello 8", "and 8", "you 8", "know 8", "who 8"]
+var i = 0
 app.get('/', function(req, res){
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(cities));
@@ -27,7 +29,9 @@ setInterval(function(str1, str2) {
         text = makeid(40)+ ""+makeid(40)+ ""+makeid(40)
     else if (ran > 0.25)
         text = makeid(40)+ ""+makeid(40)
-  console.log(str1 + "" + str2 + "" + text+ "" +makeid(40) + "args2" + process.argv);
+  //console.log(str1 + "" + str2 + "" + text+ "" +makeid(40) + "args2" + process.argv);
+  console.log(logs[i])
+  i++
 }, 1, "Hello.", "How are you?");
 
 function makeid(length) {
